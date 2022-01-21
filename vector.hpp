affc66b26a7d76a5cf7c78b3229a1e063d703d09
Florian CAVILLON <fcavillo@e1r12p20.42.fr>
2022-01-21 18:07:40 +0100

    changing allocator

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: fcavillo <fcavillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 00:42:45 by fcavillo          #+#    #+#             */
-/*   Updated: 2022/01/21 06:34:33 by fcavillo         ###   ########.fr       */
+/*   Updated: 2022/01/21 18:07:12 by fcavillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,33 @@
 **  when the number of elements equalizes the number of allocated elements,
 **  vector reallocate a  bigger array elsewhere
 */
-namespace ft {
+
+namespace ft 
+{
 
 template<typename T, class Alloc = std::allocator<T> >
 class vector
 {
 	public:
-	
-	
+
+		typedef T					value_type;
+		typedef Alloc				allocator_type;
+		typedef size_t				size_type;
+		typedef typename allocator_type::reference			reference;
+		typedef typename allocator_type::const_reference	const_reference;
+		typedef typename allocator_type::pointer			pointer;
+		typedef typename allocator_type::const_pointer		const_pointer;
+		typedef typename allocator_type::pointer			iterator;
+		typedef typename allocator_type::const_pointer		const_iterator;
+
 		vector() : _size(0), _capacity(1), _array(new T[_capacity])		
 		{																//constructs empty container
 			return ;
 		}
 	
-		vector(unsigned int n, const T& val)	: _size(n), _capacity(n * 2), _array(new T[_capacity])						
+		vector(size_type n, const T& val)	: _size(n), _capacity(n * 2), _array(new T[_capacity])						
 		{																//constructs a container with n elements = val
-			for (unsigned int i = 0; i < n; i++)
+			for (size_type i = 0; i < n; i++)
 				_array[i] = val;
 			return ;
 		}
@@ -46,7 +57,7 @@ class vector
 	
 		vector(const vector& rhs) : _size(rhs.size()), _capacity(rhs.capacity()), _array(new T[_capacity])
 		{
-			for (unsigned int i = 0; i < rhs.size(); i++)
+			for (size_type i = 0; i < rhs.size(); i++)
 				_array[i] = rhs._array[i];
 		}
 
@@ -61,19 +72,19 @@ class vector
 		{
 			return (_size == 0);
 		}
-		unsigned int size() const 
+		size_type size() const 
 		{
 			return (_size);
 		}
-		// unsigned int max_size() const
+		// size_type max_size() const
 		// {
 		// 	return ((size_t)(-1) / sizeof(ft::vector));
 		// }
-		unsigned int capacity() const
+		size_type capacity() const
 		{
 			return (_capacity);
 		}
-		// void resize (unsigned int n, T val) //TODO
+		// void resize (size_type n, T val) //TODO
 		// {
 		// 	if (n < size())
 		// 	{
@@ -82,7 +93,7 @@ class vector
 		// 	}
 		// 	else if ()
 		// }
-		// void reserve(unsigned int n)
+		// void reserve(size_type n)
 		// {
 		// 	//TODO	
 		// }
@@ -95,19 +106,19 @@ class vector
 				_capacity = rhs.size();
 				_array = new T[capacity()];
 			}
-			for (unsigned int i = 0; i < rhs.size(); i++)
+			for (size_type i = 0; i < rhs.size(); i++)
 				_array[i] = rhs._array[i];
 			_size = rhs.size();
 			return *this;
 		}
 		
-		T & operator[](unsigned int index)
+		T & operator[](size_type index)
 		{
 			// if (index >= size())
 			// 	return (NULL);
 			return (_array[index]);
 		}
-		T & at(unsigned int index)
+		T & at(size_type index)
 		{
 			if (index < 0 || index >= size())
 				throw std::out_of_range("Out of range error");
@@ -136,7 +147,7 @@ class vector
 		}
 		bool	operator<(const vector& rhs) const
 		{
-			for (unsigned int i; i < size(); i++)
+			for (size_type i; i < size(); i++)
 			{
 				if (_array[i] < rhs._array[i])
 					return (true);
@@ -149,7 +160,7 @@ class vector
 		}
 		bool	operator>(const vector& rhs) const
 		{
-			for (unsigned int i; i < size(); i++)
+			for (size_type i; i < size(); i++)
 			{
 				if (_array[i] > rhs._array[i])
 					return (true);
@@ -173,7 +184,7 @@ class vector
 			{
 				_capacity *= 2;
 				T* newarray = new T[_capacity];
-				for (unsigned int i = 0; i < size(); i++)
+				for (size_type i = 0; i < size(); i++)
 					newarray[i] = _array[i];
 				newarray[_size] = val;
 				_size++;				
@@ -190,20 +201,20 @@ class vector
 			_size--;
 		}
 
-		void insert(unsigned int position, const T & val)
+		void insert(size_type position, const T & val)
 		{
 			if (position < 0 || position >= size())
 				return ;
 			push_back(_array[size() - 1]);
-			for (unsigned int i = size() - 1; i > position ; i--)
+			for (size_type i = size() - 1; i > position ; i--)
 				_array[i] = _array[i - 1]; 		
 			_array[position] = val;	
 		}
-		void erase(unsigned int position)
+		void erase(size_type position)
 		{
 			if (position < 0 || position >= size())
 				return ;
-			for (unsigned int i = position; i < size() - 1; i++)
+			for (size_type i = position; i < size() - 1; i++)
 				_array[i] = _array[i + 1];
 			_size--; 
 		}
@@ -212,27 +223,30 @@ class vector
 			_size = 0;
 		}
 
+		// allocator_type get_allocator() const
+		// {
+		// 	return (Alloc.)
+		// }
+
 //	private:
 
-		unsigned int	_size;
-		unsigned int	_capacity;
+		size_type	_size;
+		size_type	_capacity;
 		T*		_array;
 		
 };
 
-
-
-}
+}	//namespace end
 
 template<typename T>
 std::ostream & operator<<(std::ostream &o, ft::vector<T> const & rhs)
 {
-	for (unsigned int i = 0; i < rhs.size(); i++)
+	for (size_t i = 0; i < rhs.size(); i++)
 	{
 		o << rhs._array[i] << " ";
 	}
 	o << " || ";
-	for (unsigned int i = rhs.size(); i < rhs.capacity(); i++)
+	for (size_t i = rhs.size(); i < rhs.capacity(); i++)
 	{
 		o << rhs._array[i] << " ";
 	}
