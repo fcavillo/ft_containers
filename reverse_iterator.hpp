3b0ed4deb1fdce96fa9f81f2495683b7fe987294
fcavillo <fcavillo@student.42.fr>
2022-01-29 17:49:45 +0100

    fixed iterator substraction

--- reverse_iterator.hpp
+++ reverse_iterator.hpp
@@ -1,25 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   reverse_operator.hpp                               :+:      :+:    :+:   */
+/*   reverse_iterator.hpp                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fcavillo <fcavillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 17:24:33 by fcavillo          #+#    #+#             */
-/*   Updated: 2022/01/27 17:25:04 by fcavillo         ###   ########.fr       */
+/*   Updated: 2022/01/29 17:41:24 by fcavillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef REVERSE_ITERATOR_HPP
-#define REVERSE_OPERATOR_HPP
+#define REVERSE_ITERATOR_HPP
 
 namespace ft
 {
 
+template <typename Iterator>
+class   reverse_iterator
+{
+    public :
 
+  
+
+    
+};
 
 
     
-}
+};
 
 #endif
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: fcavillo <fcavillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 00:42:45 by fcavillo          #+#    #+#             */
-/*   Updated: 2022/01/27 17:20:24 by fcavillo         ###   ########.fr       */
+/*   Updated: 2022/01/29 17:48:17 by fcavillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,8 @@ class vector
 		typedef typename allocator_type::const_pointer		const_pointer;
 		typedef typename allocator_type::pointer			iterator;
 		typedef typename allocator_type::const_pointer		const_iterator;
-		typedef ft::reverse_iterator<iterator>				reverse_iterator;
-		typedef ft::reverse_iterator<const_iterator>		const_reverse_iterator;
+	//	typedef ft::reverse_iterator<iterator>				reverse_iterator;
+	//	typedef ft::reverse_iterator<const_iterator>		const_reverse_iterator;
 
 	/*	CONSTRUCTOR, DESTRUCTOR, OPERATOR=	*/
 
@@ -76,7 +76,6 @@ class vector
 				_alloc.construct(_array + _size, val);
 				_size++;
 			}
-//			assign(n, val);
 		}
 
 
@@ -87,8 +86,10 @@ class vector
 		_alloc(alloc),
 		_size(0)
 		{
-			size_t	s = last - first;
-
+			size_t	s = 0;
+			
+			for (InputIterator tmp = first; tmp != last; tmp++)
+				s++;
 			_array = _alloc.allocate(s);
 			_capacity = s;
 
